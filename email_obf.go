8dd49ae98caf7057f4e2654599a60c84cec2b117
itdao6 [Intan.Maryam] <p130420026@bussan.co.id>
2021-09-23 15:36:04 +0700

    [UPDATE] add more condition; add scenario testing

--- email_obf.go
+++ email_obf.go
@@ -5,15 +5,17 @@ import (
 )
 
 // ObfuscateEmail is a function to modify email show in public with asterisk
-func ObfuscateEmail(locpart, domain string) string {
+func ObfuscateEmail(email string) string {
+	conv := strings.Split(email, "@")
+
 	// Prepare data
-	llocpart := len(locpart)       // Get length of local-part
-	obf := locpart[2 : llocpart-1] // Get local-part that we will modify into asterisk (obfuscated)
+	llocpart := len(conv[0])       // Get length of local-part
+	obf := conv[0][2 : llocpart-1] // Get local-part that we will modify into asterisk (obfuscated)
 	lobf := len(obf)               // Get length of local-part that will be obfuscated
 
-	// If length local-part less than 5 chars, it's needed because in microsoft it's allowed
-	if llocpart < 5 {
-		obf = locpart[1 : llocpart-1]
+	// If length local-part less than 6 chars, it's needed because in microsoft it's allowed
+	if llocpart < 6 {
+		obf = conv[0][1 : llocpart-1]
 		lobf = len(obf)
 	}
 
@@ -21,7 +23,7 @@ func ObfuscateEmail(locpart, domain string) string {
 	ast := strings.Repeat("*", lobf)
 
 	// Replace it
-	localpart := strings.Replace(locpart, obf, ast, -1)
+	localpart := strings.Replace(conv[0], obf, ast, -1)
 
-	return localpart + "@" + domain
+	return localpart + "@" + conv[1]
 }
